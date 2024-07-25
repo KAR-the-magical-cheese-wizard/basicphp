4a2b5b884c3454b2359d7df36f8414d6e6bb0857
karneson sommoh <akuykub@gmail.com>
2024-07-25 10:47:14 +0700

    [Create] register

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if (session_id() != $_SESSION["sess_id"]) {
+    header("Location: login.php");
+
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="register.php">All user</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+  <li style="float:right">
+    <a class="active" href="logout.php">Logout</a>
+
+</li>
+</ul>
+</ul>
+
+</body>
+</html>
